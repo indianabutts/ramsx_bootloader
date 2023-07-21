92cc722c170ea004f387614b673abaea85249277
Yassir Al-Refaie <yassir.alrefaie@gmail.com>
2023-07-21 19:09:37 +0100

    Cleaned up build script and added docs

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,2 +1,2 @@
-~/dev/assembler/vasm/vasmz80_oldstyle ./MAIN.asm -chklabels -nocase -Dvasm=1 -DBuildMSX=1 -DBuildMSX_MSX1=1 -L out.sym -Fbin -o out.rom
-/Applications/openMSX.app/Contents/MacOS/openmsx -cart out.rom
+vasmz80_oldstyle ./main.asm -chklabels -nocase -Dvasm=1 -DBuildMSX=1 -DBuildMSX_MSX1=1 -L out.sym -Fbin -o out.rom
+openmsx -cart out.rom

af609e72f490a862c2cec4172e6272303f03229f
Yassir Al-Refaie <yassir@152.3.123.172.rev.iijmobile.jp>
2023-09-24 13:07:50 +0100

    Tidy up and condense Input Reading to single file
    Also move the file into the Utils folder

--- src/utils/INPUT_FUNCTIONS.asm
+++ src/utils/INPUT_FUNCTIONS.asm
@@ -1,15 +1,29 @@
-Input_UpdateInputBuffers:
+	/*
+==========================
+	Input Variables
+==========================
+	*/
+INPUT_STATE:	equ $C020
+INPUT_PREV_STATE:	equ $C030
+
+Input:
+	/*
+	===============================================
+	Update Input State Buffers
+	Registers - All
+	===============================================
+	*/
+.UpdateInputBuffers:	
 	;; First we need to copy the last current state, to the previous state buffer
 	ld bc, 10
 	ld hl, INPUT_STATE
 	ld de, INPUT_PREV_STATE
 	ldir
 	;; Then we want to go through each row and copy them to INPUT_STATE + Row number
-
 	;; Setup Loop
 	ld hl, INPUT_STATE
 	ld c,0
-_Input_UpdateInputBuffers_Loop:	
+.Loop:
 	ld a, c
 	call SNSMAT
 	ld (hl), a
@@ -17,23 +31,6 @@ _Input_UpdateInputBuffers_Loop:
 	inc c
 	ld a, c
 	cp 11
-	jr nz, _Input_UpdateInputBuffers_Loop
+	jr nz, .Loop
 	;; TODO: Add Logic for handling key being held down.
 	ret
-	
-_Input_ClearRepeat:	
-	ld a, 0
-	ld (INPUT_CUR_REP_COUNT), a
-	ret
-	
-_Input_IncrementRep:
-	ld a, (INPUT_CUR_REP_COUNT)
-	inc a
-	cp INPUT_REP_COUNT
-	jr z, _Input_IncrementRep_Over
-	ret
-_Input_IncrementRep_Over:
-	ld a, 0
-	ld (INPUT_CUR_REP_COUNT), a
-	ret
-	

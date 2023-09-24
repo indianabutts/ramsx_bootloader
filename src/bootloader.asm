56eb4c19f6c6aef08856e6ad1a9ea60f194e27ce
Yassir Al-Refaie <yassir@152.3.123.172.rev.iijmobile.jp>
2023-09-24 18:08:46 +0100

    Move old main code to browse_state

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -5,33 +5,15 @@
 	DEFINE ORGADDR $4000	; Origin for the ROM
 	
 
-	include "sys/SYS_BIOSCALLS.asm"
-	include "sys/SYS_VARIABLES.asm"
+	include "sys/bios_calls.asm"
+	include "sys/variables.asm"
 	
-		/*
-	include "utils/CHAR_CODES.asm"
-	include "cursor/CURSOR_VARIABLES.asm"
-	include "vram/VRAM_VARIABLES.asm"
-	include "input/INPUT_VARIABLES.asm"
-
-	include "command/COMMAND_CONSTANTS.asm"
-	include "input/INPUT_CONSTANTS.asm"
-	include "vram/VRAM_CONSTANTS.asm"
-	*/
-	include "utils/CART_HEADER.asm"
+	include "utils/cart_header.asm"
 	dw BROWSE_STATE_DISPLAY
-	;; 	include "vram/VRAM_BUFFER.asm" 
 	
 TOTAL_PAGES:
 	dw 30
-	/*
-	include "cursor/CURSOR_FUNCTIONS.asm"
-	;; 	include "vram/VRAM_FUNCTIONS.asm"
-	include "utils/MATH_FUNCTIONS.asm"
-
-	include "command/COMMAND_FUNCTIONS.asm"
-*/
-	include "utils/INPUT_FUNCTIONS.asm"
+	include "utils/input_functions.asm"
 
 				; ==[ Program ]=============================================
 Init:
@@ -41,44 +23,20 @@ Init:
 	ld (BDCLR), a
 	ld a, 0
 	call CHGCLR
+	ld a, 10
+	call WaitSeconds
 	ld hl, SEARCH_STATE_DISPLAY
-	ld de, (TXTNAM)
-	ld bc, 960
-	call LDIRVM
+	call VRAM.CopyBufferToRAM
+	call VRAM.CopyWorkBufferToVDP
 
 MainLoopTest:
 	call Input.UpdateInputBuffers
 	jr MainLoopTest
-/*
-InitTemp:
-	;; Set Screen Mode to 0
-	call INITXT
-	;; Set Pallette to White on Black
-	ld a, 1
-	ld (BAKCLR), a
-	ld (BDCLR), a
-	ld a, 0
-	ld (CUR_INDEX),a
-	ld (OLD_CUR_INDEX),a
-	call CHGCLR
-	call Command_CopyProgramFunctionsToRAM
-	;; Copy the VRAM_BUFFER to RAM so that we can update it
-	call VRAM_CopyBufferToRam
-	;; Copy the VRAM_BUFFER in RAM to VRAM
-	call VRAM_CopyWorkBufferToVDP
-	call MainLoop
-
-MainLoop:
-	di
-	call Input_UpdateInputBuffers
-	call Cursor_CheckInput
-	call Command_CheckInput
-	jr MainLoop
-*/
-	include "command/COMMAND_DATA.asm"
-	include "command/COMMAND_VARIABLES.asm"
+	
 	include "states/splash_state/splash_state.asm"
 	include "states/browse_state/browse_state.asm"
 	include "states/search_state/search_state.asm"
 	include "states/search_result_state/search_result_state.asm"
 	include "states/settings_state/settings_state.asm"
+        include "utils/util_functions.asm"
+	include "utils/gfx_functions.asm"
--- src/states/browse_state/browse_state.asm
+++ src/states/browse_state/browse_state.asm
@@ -1,1 +1,30 @@
 	include "browse_state_screens.asm"
+/*
+InitTemp:
+	;; Set Screen Mode to 0
+	call INITXT
+	;; Set Pallette to White on Black
+	ld a, 1
+	ld (BAKCLR), a
+	ld (BDCLR), a
+	ld a, 0
+	ld (CUR_INDEX),a
+	ld (OLD_CUR_INDEX),a
+	call CHGCLR
+	call Command_CopyProgramFunctionsToRAM
+	;; Copy the VRAM_BUFFER to RAM so that we can update it
+	call VRAM_CopyBufferToRam
+	;; Copy the VRAM_BUFFER in RAM to VRAM
+	call VRAM_CopyWorkBufferToVDP
+	call MainLoop
+
+MainLoop:
+	di
+	call Input_UpdateInputBuffers
+	call Cursor_CheckInput
+	call Command_CheckInput
+	jr MainLoop
+
+	include "command/COMMAND_DATA.asm"
+	include "command/COMMAND_VARIABLES.asm"
+	*/

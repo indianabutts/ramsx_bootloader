9e310975a37b17b72a226a272fc79c3a5b89e1a5
Yassir Al-Refaie <yassir.alrefaie@gmail.com>
2025-01-30 01:00:34 +0000

    Added Custom Font

---
+++ src/utils/font.asm
@@ -0,0 +1,10 @@
+FONT_SET:	
+	db $00, $00, $00, $00, $00, $00, $00, $00 ; Space
+	db $00, $00, $00, $00, $00, $00, $00, $00 ; Space
+	db $3C, $7E, $DB, $DB, $BD, $C3, $7E, $3C ; Face Outline
+	db $6C, $AA, $92, $44, $44, $28, $10, $00 ; Face Filled
+	
+	db $10, $28, $44, $82, $44, $28, $10, $00 ; Heart Outine
+	db $10, $38, $38, $D6, $FE, $D6, $38, $00 ; Diamond Outline
+	db $
+
